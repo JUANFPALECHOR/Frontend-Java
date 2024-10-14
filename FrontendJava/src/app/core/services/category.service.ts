013ff229f451984590dc3d3278d137043fc31a6d
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-10-14 14:52:48 -0500

    feat(categoría): agregar funcionalidad de paginación y ordenación al servicio de categorías

--- FrontendJava/src/app/core/services/category.service.ts
+++ FrontendJava/src/app/core/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 //Define las rutas de la API en los servicios 
@@ -17,9 +17,39 @@ export class CategoryService {
     return this.http.post<void>(this.apiUrl, category);
   }
 
+  // Método actualizado para obtener categorías con paginación y orden
+  getCategories(page: number, size: number, sort: 'asc' | 'desc'): Observable<CategoryResponse[]> {
+    // Construir parámetros de consulta
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('sort', sort);
+
+    // Hacer la solicitud HTTP con los parámetros
+    return this.http.get<CategoryResponse[]>(`${this.apiUrl}`, { params });
+  }
+
 }
 
+
+// Esta interfaz representa el objeto que envías para crear una nueva categoría.
 export interface CategoryRequest {
   name: string;
   description: string;
+}
+
+// Esta interfaz representa una categoría individual.
+export interface Category {
+  id: number;
+  name: string;
+  description: string;
+}
+
+
+// Esta interfaz es para manejar la respuesta paginada de categorías.
+export interface CategoryResponse {
+  categories: Category[]; // Lista de categorías
+  totalElements: number;  // Total de categorías
+  totalPages: number;     // Total de páginas
+  currentPage: number;    // Página actual
 }
