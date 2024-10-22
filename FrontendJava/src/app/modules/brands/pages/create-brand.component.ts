2df7b70b9c26adb968e2c48a01d22d7e7f4afdac
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-10-21 23:46:29 -0500

    agregar funcionalidad para crear marcas (Brand) con validación y notificación de éxito

--- FrontendJava/src/app/modules/brands/pages/create-brand.component.ts
+++ FrontendJava/src/app/modules/brands/pages/create-brand.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { BrandService } from '../../../core/services/brand.service';  
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NotificationComponent } from './../../atomic-design/atoms/notification/notification.component';
 
 @Component({
   selector: 'app-create-brand',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-brand.component.scss']
 })
 export class CreateBrandComponent implements OnInit {
+  @ViewChild(NotificationComponent) notificationComponent!: NotificationComponent;
+  brandForm!: FormGroup;  // Formulario para crear marcas (Brand)
 
-  constructor() { }
+  constructor(
+    private readonly formBuilder: FormBuilder,  // Constructor para crear el formulario
+    private readonly brandService: BrandService,  // Servicio para manejar marcas
+    private readonly router: Router  // Router para posibles redirecciones
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();  // Inicializa el formulario en la carga del componente
   }
 
+  // Inicializa el formulario con validaciones para los campos 'name' y 'description'
+  initializeForm(): void {
+    this.brandForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+      description: ['', [Validators.required, Validators.maxLength(90)]]
+    });
+  }
+
+  // Lógica al enviar el formulario para crear una nueva marca
+  onSubmit(): void {
+    if (this.brandForm?.valid) {
+      console.log('Formulario enviado', this.brandForm.value);
+      this.brandService.createBrand(this.brandForm.value).subscribe({
+        next: (response) => {
+          this.notificationComponent.show('Marca creada con éxito', 'success');
+        },
+        error: (error) => {
+          console.error('Error al crear la marca:', error);
+        }
+      });
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
