a0e9eb6ccf2bfb5e8ea036c76da1d7870760684b
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-10-21 23:57:43 -0500

    feat: Implementación de formulario de creación de marcas
    - Se agregó el formulario para crear una nueva marca con validación.
    - Se reutilizan los componentes de Atomic Design (app-input, app-textarea, app-button).
    - Se incluye un componente de notificación para mostrar mensajes de éxito al crear una marca.

--- FrontendJava/src/app/modules/brands/brand.module.ts
+++ FrontendJava/src/app/modules/brands/brand.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AtomicModule } from '../atomic-design/atomic.module';
 
-// Importar los componentes para Brand
+
 import { CreateBrandComponent } from './pages/create-brand.component';
 
 
-// Definir las rutas específicas para este módulo
+
 const routes: Routes = [
-  { path: 'new', component: CreateBrandComponent },   // Ruta para crear una nueva marca       
+  { path: 'new', component: CreateBrandComponent },         
 ];
 
 @NgModule({
   declarations: [
-    CreateBrandComponent,  // Componente para crear una nueva marca
+    CreateBrandComponent,  
   ],
   imports: [
     CommonModule,
-    FormsModule,           // Para manejar formularios
-    ReactiveFormsModule,    // Para manejar formularios reactivos
-    RouterModule.forChild(routes)  // Configuración de las rutas específicas del módulo
+    FormsModule,          
+    ReactiveFormsModule,   
+    RouterModule.forChild(routes),  
+    AtomicModule
   ]
 })
 export class BrandModule { }
